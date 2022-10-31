53e26a5390fa6f9bdb6218ec967cc3f63c79b7fa
Pratik Sutar <sutarpratik2012@gmail.com>
2022-10-31 17:03:57 +0900

    imp
    Former-commit-id: 564582bf6ed83d6c9c6e083956fc34bde9383b8c

--- test_audiofeatures.py
+++ test_audiofeatures.py
@@ -1,15 +1,23 @@
 import sys
+from pydub import AudioSegment, silence
+import os
+from pathlib import Path
 
 import numpy as np
-from ddsp.colab.colab_utils import audio_bytes_to_np
+from .ddsp.colab.colab_utils import audio_bytes_to_np
 
 audio_file_path = sys.argv[1]
 sample_rate = 16000
 
+### ###
+
+
 print(f"Loading file: {audio_file_path}")
 
+audio = AudioSegment.from_wav(audio_file_path).set_channels(1)
 
-audio = audio_bytes_to_np(audio_file_path,
+
+audio = audio_bytes_to_np(audio,
                                  sample_rate=sample_rate,
                                  normalize_db=None)
 
