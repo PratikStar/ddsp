f3a7c06dc31b3cce2b622a8ee26fe5aaa401bcbe
Pratik Sutar <sutarpratik2012@gmail.com>
2022-12-21 21:16:10 +0900

    add 4096 spectral loss fft

--- ddsp/losses.py
+++ ddsp/losses.py
@@ -142,7 +142,7 @@ class SpectralLoss(Loss):
   """
 
   def __init__(self,
-               fft_sizes=(2048, 1024, 512, 256, 128, 64),
+               fft_sizes=(4096, 2048, 1024, 512, 256, 128, 64),
                loss_type='L1',
                mag_weight=1.0,
                delta_time_weight=0.0,
@@ -196,8 +196,6 @@ class SpectralLoss(Loss):
 
   def call(self, target_audio, audio, *args, weights=None):
     logging.debug(f"In SpectralLoss.call")
-    logging.debug(f"\tpred: {audio}")
-    logging.debug(f"\ttarget: {target_audio}")
 
     loss = 0.0
 
