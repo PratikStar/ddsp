50acba2740fc7d0a4ad6e3edb60ee796420435ab
Pratik Sutar <sutarpratik2012@gmail.com>
2022-12-21 15:15:53 +0900

    bugfix

--- ddsp/training/train_util.py
+++ ddsp/training/train_util.py
@@ -23,8 +23,9 @@ from ddsp.training import cloud
 import gin
 import tensorflow.compat.v2 as tf
 import wandb
-import ddsp.colab.colab_utils as colab_utils
 
+import note_seq
+import numpy as np
 # ---------------------- Helper Functions --------------------------------------
 def get_strategy(tpu='', cluster_config=''):
   """Create a distribution strategy for running on accelerators.
@@ -348,7 +349,7 @@ def train(data_provider,
 
           sample_rate = trainer.model.preprocessor.sample_rate
           # save the harmonic and noise clips
-          harmonic_output = colab_utils.audio_bytes_to_np(['harmonic']['signal'].numpy(), sample_rate=sample_rate)
+          harmonic_output = audio_bytes_to_np(['harmonic']['signal'].numpy(), sample_rate=sample_rate)
           wandb.log(
             {f"harmonic-{step}": wandb.Audio(harmonic_output, caption=f"harmonic-{step}", sample_rate=sample_rate)})
 
@@ -370,3 +371,24 @@ def train(data_provider,
     summary_writer.flush()
 
   logging.info('Training Finished!')
+
+def audio_bytes_to_np(wav_data,
+                      sample_rate=DEFAULT_SAMPLE_RATE,
+                      normalize_db=0.1,
+                      mono=True):
+  """Convert audio file data (in bytes) into a numpy array using Pydub.
+
+  Args:
+    wav_data: A byte stream of audio data.
+    sample_rate: Resample recorded audio to this sample rate.
+    normalize_db: Normalize the audio to this many decibels. Set to None to skip
+      normalization step.
+    mono: Force stereo signals to single channel. If false, output can one or
+      two channels depending on the source signal.
+
+  Returns:
+    An array of the recorded audio at sample_rate, shape [channels, time].
+  """
+  return note_seq.audio_io.wav_data_to_samples_pydub(
+    wav_data=wav_data, sample_rate=sample_rate, normalize_db=normalize_db,
+    num_channels=1 if mono else None)
