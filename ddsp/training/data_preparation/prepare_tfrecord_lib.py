a02976895d259723de5d41c8cbe542f09275eb66
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-08 23:30:19 +0900

    debugging

--- ddsp/training/data_preparation/prepare_tfrecord_lib.py
+++ ddsp/training/data_preparation/prepare_tfrecord_lib.py
@@ -112,6 +112,7 @@ def _get_f0_estimate_from_di(ex, frame_rate, center, viterbi):
             di_audio, frame_rate, viterbi=viterbi, padding=padding)
 
         logging.debug(f"Fo Confidence: {f0_confidence.shape}")
+        logging.debug(f"confidence mean: {}")
         di_f0_estimates[di_audio_path] = {
             'f0_hz': f0_hz,
             'f0_confidence': f0_confidence
